1020abc56efd3667a28be42ac3626e9b3971fc4a
Evgeny Lityushkin <evgeny.lityushkin@gmail.com>
2017-08-08 11:33:55 +0300

    Create sslcheck-issuer-cn.bash

---
+++ sslcheck-issuer-cn.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Simple SSL cert get-issuer-CN
+# by Glen Scott, www.glenscott.net
+
+openssl_output=$(echo ”
+GET / HTTP/1.0
+EOT” \
+| openssl s_client -connect $1:443 2>&1);
+
+if [[ “$openssl_output” = *”—–BEGIN CERTIFICATE—–“* ]]; then
+
+cert_issuer=$(echo “$openssl_output” \
+| sed -n ‘/—–BEGIN CERTIFICATE—–/,/—–END CERTIFICATE—–/p’ \
+| openssl x509 -noout -issuer -nameopt sname \
+| tr ‘/’ ‘\n’ | grep CN= | cut -c4- );
+
+echo “$cert_issuer”;
+exit 0
+
+else
+echo “NOT_FOUND”;
+exit 1
+fi
